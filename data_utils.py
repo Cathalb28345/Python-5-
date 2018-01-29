ffd085a3de6781412ffd63c45f6da1480710d4c1
Cathal Brady <cathalb4@live.com>
2018-01-29 15:38:25 +0000

    This is the first version of my webapp Git test

---
+++ data_utils.py
@@ -0,0 +1,38 @@
+# import DBcm
+
+# config = {
+#     'user': 'scoreuser',
+#     'password': 'scorepasswd',
+#     'host': 'localhost',  # Change this on PythonAnywhere.
+#     'database': 'scoresDB',
+# }
+
+
+import pymongo
+
+
+def add_to_scores(name, score, word, list_of_words) -> None:
+    """Add the name and its associated score to the database, as well
+       as the word (which was the sourseword), and also the list of
+       words that the user got right."""
+    score = {
+        'name': name,
+        'score': score,
+        'sourceword': word,
+        'words': list_of_words,
+    }
+    client = pymongo.MongoClient()
+    db = client['leadersDB']
+    c = db['leaderboard']
+    c.insert_one(score)
+
+
+def get_sorted_leaderboard() -> list:
+    """Return a sorted list of tuples - this is the leaderboard."""
+    client = pymongo.MongoClient()
+    db = client['leadersDB']
+    c = db['leaderboard']
+    results = []
+    for s in c.find().sort('score', pymongo.ASCENDING):
+        results.append( ( s['score'], s['name'], s['sourceword'], s['words'] ) )
+    return results
